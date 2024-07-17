7c6185aaab2d84ca528f514c371c61ea404cb938
NHPT <42366434+NHPT@users.noreply.github.com>
2024-07-18 00:49:24 +0800

    Create jsonoutput.go

---
+++ output/jsonoutput.go
@@ -0,0 +1,15 @@
+package output
+
+import (
+    "encoding/json"
+    "io/ioutil"
+    "hfinger/config"
+)
+
+func WriteJSONOutput(filename string, results []config.Result) error {
+    data, err := json.MarshalIndent(results, "", "  ")
+    if err != nil {
+        return err
+    }
+    return ioutil.WriteFile(filename, data, 0644)
+}
