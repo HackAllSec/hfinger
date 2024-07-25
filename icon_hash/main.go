1661113e7dc194df6cadcdb3e8b09a3fcde17fa8
NHPT <42366434+NHPT@users.noreply.github.com>
2024-07-25 11:24:28 +0800

    Update main.go

--- icon_hash/main.go
+++ icon_hash/main.go
@@ -3,54 +3,34 @@ package main
 import (
     "flag"
     "fmt"
-    "io/ioutil"
-    "crypto/tls"
-    "net/http"
+
     "hfinger/models"
 )
 
 func main() {
-    url := flag.String("u", "", "The URL to process.")
-    base64data := flag.String("i", "", "The base64 data of favicon.")
+    url := flag.String("u", "", "The Target icon file, example: url, localfile.")
     flag.Usage = func() {
         fmt.Println("Usage:")
-        fmt.Println("    icon_hash -u <URL>")
+        fmt.Println("    icon_hash -u <Target>")
         flag.PrintDefaults()
         fmt.Println("Example:")
         fmt.Println("    icon_hash -u https://www.example.com/favicon.ico")
+        fmt.Println("    icon_hash -u favicon.ico")
+        fmt.Println("    icon_hash -u https://www.example.com/login")
     }
 
     flag.Parse()
 
-    if *url == "" && *base64data == ""{
+    if *url == ""{
         flag.Usage()
         return
     }
-    client := &http.Client{
-        Transport: &http.Transport{
-            TLSClientConfig: &tls.Config{
-                InsecureSkipVerify: true,
-            },
-        },
-    }
 
     if *url != "" {
-        resp, err := client.Get(*url)
+        icon_hash, err := models.IconHash(*url)
         if err != nil {
-            fmt.Printf("[!] Error: %v",err)
+            fmt.Println(err.Error())
         }
-        defer resp.Body.Close()
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            fmt.Printf("[!] Error: %v",err)
-        }
-        icon_hash := models.Mmh3Hash32(models.StandBase64(body))
         fmt.Println("[+]The icon_hash is:",icon_hash)
     }
-    if *base64data != "" {
-        icondata := []byte(*base64data)
-        icon_hash := models.Mmh3Hash32(icondata)
-        fmt.Println("[+]The icon_hash is:",icon_hash)
-    }
-    
 }
