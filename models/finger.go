86dbac1055d53fae502031d1d86cefb3f362018e
NHPT <42366434+NHPT@users.noreply.github.com>
2024-08-21 10:05:07 +0800

    Update finger.go

--- models/finger.go
+++ models/finger.go
@@ -2,7 +2,8 @@ package models
 
 import (
     "fmt"
-    "io/ioutil"
+    "io"
+    "os"
     "net/http"
     "strings"
     "sync"
@@ -18,139 +19,150 @@ import (
 var (
     workerCount int64
     resultsLock sync.Mutex
+    cfg *config.FingerprintConfig = config.GetConfig()
 )
 
-func MatchFingerprint(body []byte, header map[string][]string, title string, favicon []byte) (string, bool) {
-    cfg := config.GetConfig()
-    for _, fingerprint := range cfg.Finger {
-        matched := matchKeywords(body, header, title, favicon, fingerprint)
-        if matched {
-            return fingerprint.CMS, true
+func process(url string, headers map[string]string, resultsChannel chan<- config.Result, matchedCMS *sync.Map, mu *sync.Mutex, wg *sync.WaitGroup, errOccurred *bool) {
+    defer wg.Done()
+
+    mu.Lock()
+    if *errOccurred {
+        mu.Unlock()
+        return
+    }
+    mu.Unlock()
+    
+    resp, err := utils.Get(url, headers)
+    if err != nil {
+        mu.Lock()
+        if !*errOccurred {
+            *errOccurred = handleError(err, url)
+        }
+        mu.Unlock()
+        return
+    }
+    defer resp.Body.Close()
+
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        color.Red("[%s] [!] Error: %s", time.Now().Format("01-02 15:04:05"), err)
+        return
+    }
+
+    title := utils.FetchTitle(body)
+    if title == "" {
+        title = "None"
+    }
+    faviconpath := utils.FetchFavicon(body)
+    var faviconbody []byte
+    if faviconpath != "" && resp.StatusCode == http.StatusOK {
+        baseurl, _ := utils.GetBaseURL(url)
+        faviconurl := baseurl + "/" + faviconpath
+        if strings.HasPrefix(faviconpath, "http://") || strings.HasPrefix(faviconpath, "https://") {
+            faviconurl = faviconpath
+        }
+        if faviconpath[0] == '/' {
+            faviconurl = baseurl + faviconpath
+        }
+        favicon, err := utils.Get(faviconurl, nil)
+        if err == nil && favicon.StatusCode == http.StatusOK {
+            defer favicon.Body.Close()
+            faviconbody, err = io.ReadAll(favicon.Body)
+            if err != nil {
+                color.Yellow("[%s] [-] Warning: %s", time.Now().Format("01-02 15:04:05"), err)
+            }
         }
     }
-    return "Not Matched", false
+
+    statuscode := resp.StatusCode
+    server := resp.Header.Get("Server")
+    if server == "" {
+        server = "None"
+    }
+    for _, fingerprint := range cfg.Finger {
+        ismatched := matchKeywords(body, resp.Header, title, faviconbody, fingerprint)
+        if ismatched {
+            cms := fingerprint.CMS
+            result := config.Result{
+                URL:        url,
+                CMS:        cms,
+                Server:     server,
+                StatusCode: statuscode,
+                Title:      title,
+            }
+
+            if _, loaded := matchedCMS.LoadOrStore(cms, true); !loaded {
+                resultsChannel <- result
+                color.Green("[%s] [+] [%s] [%s] [%d] [%s] [%s]", time.Now().Format("01-02 15:04:05"), url, cms, statuscode, server, title)
+            }
+        }
+    }
+    switch resp.StatusCode {
+    case http.StatusMovedPermanently, http.StatusFound, http.StatusSeeOther, http.StatusTemporaryRedirect:
+        location := resp.Header.Get("Location")
+        baseurl, _ := utils.GetBaseURL(url)
+        if !strings.HasPrefix(location, "http://") && !strings.HasPrefix(location, "https://") {
+            location = baseurl + location
+        }
+        wg.Add(1)
+        go process(location, nil, resultsChannel, matchedCMS, mu, wg, errOccurred)
+    default:
+        return
+    }
 }
 
 func ProcessURL(url string) {
     var wg sync.WaitGroup
     var mu sync.Mutex
     var errOccurred bool
-    var matchedCMS map[string]bool = make(map[string]bool)
-    var cms, server, title string
-    statuscode := 0
+    var matchedCMS sync.Map
     resultsChannel := make(chan config.Result, workerCount)
 
-    process := func(url string, headers map[string]string) {
-        defer wg.Done()
-
-        if errOccurred {
-            return
-        }
-
-        resp, err := utils.Get(url, headers)
-        if err != nil {
-            mu.Lock()
-            if !errOccurred {
-                if handleError(err, url) {
-                    errOccurred = true
-                }
-            }
-            mu.Unlock()
-            return
-        }
-        defer resp.Body.Close()
-
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            color.Red("[%s] [!] Error: %s", time.Now().Format("01-02 15:04:05"), err)
-            return
-        }
-
-        title = utils.FetchTitle(body)
-        if title == "" {
-            title = "None"
-        }
-        faviconpath := utils.FetchFavicon(body)
-        var faviconbody []byte
-        if faviconpath != "" && resp.StatusCode == http.StatusOK {
-            baseurl, _ := utils.GetBaseURL(url)
-            faviconurl := baseurl + "/" + faviconpath
-            if strings.HasPrefix(faviconpath, "http://") || strings.HasPrefix(faviconpath, "https://") {
-                faviconurl = faviconpath
-            }
-            if faviconpath[0] == '/' {
-                faviconurl = baseurl + faviconpath
-            }
-            favicon, err := utils.Get(faviconurl, nil)
-            if err == nil && favicon.StatusCode == http.StatusOK {
-                defer favicon.Body.Close()
-                faviconbody, err = ioutil.ReadAll(favicon.Body)
-                if err != nil {
-                    color.Yellow("[%s] [-] Warning: %s", time.Now().Format("01-02 15:04:05"), err)
-                }
-            }
-        }
-
-        var ismatched bool
-        cms, ismatched = MatchFingerprint(body, resp.Header, title, faviconbody)
-        statuscode = resp.StatusCode
-        server = resp.Header.Get("Server")
-        if server == "" {
-            server = "None"
-        }
-        result := config.Result{
-            URL:        url,
-            CMS:        cms,
-            Server:     server,
-            StatusCode: statuscode,
-            Title:      title,
-        }
-
-        mu.Lock()
-        defer mu.Unlock()
-
-        if ismatched {
-            if !matchedCMS[cms] {
-                matchedCMS[cms] = true
-                resultsChannel <- result
-                color.Green("[%s] [+] [%s] [%s] [%d] [%s] [%s]", time.Now().Format("01-02 15:04:05"), url, cms, statuscode, server, title)
-            }
-        }
-    }
-
     wg.Add(2)
-    go process(url, nil)
-    go process(url, map[string]string{"Cookie": "rememberMe=1"})
+    go process(url, nil, resultsChannel, &matchedCMS, &mu, &wg, &errOccurred)
+    go process(url, map[string]string{"Cookie": "rememberMe=1"}, resultsChannel, &matchedCMS, &mu, &wg, &errOccurred)
     wg.Wait()
 
     wg.Add(1)
-    suffix := fmt.Sprintf("/%d", rand.Int())
+    suffix := fmt.Sprintf("/%x", rand.Int())
     if url[len(url)-1] == '/' {
-        suffix = fmt.Sprintf("%d", rand.Int())
+        suffix = fmt.Sprintf("%x", rand.Int())
     }
     url = url + suffix
-    go process(url, nil)
+    go process(url, nil, resultsChannel, &matchedCMS, &mu, &wg, &errOccurred)
     wg.Wait()
 
     close(resultsChannel)
-    mu.Lock()
-    defer mu.Unlock()
 
-    if len(matchedCMS) == 0 && !errOccurred {
-        color.White("[%s] [*] [%s] [%s] [%d] [%s] [%s]", time.Now().Format("01-02 15:04:05"), url, cms, statuscode, server, title)
+    var results []config.Result
+    for result := range resultsChannel {
+        results = append(results, result)
     }
 
     resultsLock.Lock()
     defer resultsLock.Unlock()
-
-    for result := range resultsChannel {
-        //results = append(results, result)
+    for _, result := range results {
         output.AddResults(result)
     }
     err := output.WriteOutputs()
     if err != nil {
         color.Red("[%s] [!] Error: %s", time.Now().Format("01-02 15:04:05"), err)
     }
+
+    mu.Lock()
+    defer mu.Unlock()
+    if countItems(&matchedCMS) == 0 && !errOccurred {
+        color.White("[%s] [*] [%s] [None] [%d] [None] [None]", time.Now().Format("01-02 15:04:05"), url, 0)
+    }
+}
+
+func countItems(m *sync.Map) int {
+    count := 0
+    m.Range(func(_, _ interface{}) bool {
+        count++
+        return true
+    })
+    return count
 }
 
 func handleError(err error, url string) bool {
@@ -172,7 +184,7 @@ func handleError(err error, url string) bool {
 }
 
 func ProcessFile(filePath string) {
-    data, err := ioutil.ReadFile(filePath)
+    data, err := os.ReadFile(filePath)
     if err != nil {
         color.Red("[%s] [!] Error: %s", time.Now().Format("01-02 15:04:05"), err)
         return
