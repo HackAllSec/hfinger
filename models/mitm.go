0ccd244db9506761a6f0d45ebba95c1ffc0bf70a
NHPT <42366434+NHPT@users.noreply.github.com>
2024-08-21 14:25:40 +0800

    Update mitm.go

--- models/mitm.go
+++ models/mitm.go
@@ -4,12 +4,14 @@ import (
     "bufio"
     "bytes"
     "crypto/tls"
+    "fmt"
     "io"
     "net"
     "net/http"
     "net/http/httputil"
     "time"
     "strings"
+    "sync"
     "compress/gzip"
     "compress/zlib"
 
@@ -20,7 +22,8 @@ import (
 )
 
 var (
-    matchedCMS map[string]bool = make(map[string]bool)
+    matchedCMS sync.Map
+    certCache = sync.Map{}
 )
 
 func MitmServer(listenAddr string) {
@@ -92,68 +95,122 @@ func headersToMap(headers http.Header) map[string]string {
 
 // handleHTTP
 func handleHTTP(conn net.Conn, req *http.Request) error {
-    var resp *http.Response
-    url := req.URL.String()
+    color.White("[%s] [*] Received HTTP request: %s", time.Now().Format("01-02 15:04:05"), req.URL.String())
+    err := ForwardHTTPRequest(conn, req, false)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+// handleHTTPS
+func handleHTTPS(conn net.Conn, req *http.Request) error {
+    defer conn.Close()
+
+    host := req.URL.Host
+    hostParts := strings.Split(host, ":")
+    if len(hostParts) > 0 {
+        host = hostParts[0]
+    }
+
+    tlsConfig, err := getTLSConfigForHost(host)
+    if err != nil {
+        return err
+    }
+
+    _, err = conn.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
+    if err != nil {
+        return err
+    }
+
+    tlsConn := tls.Server(conn, tlsConfig)
+    err = tlsConn.Handshake()
+    if err != nil {
+        return err
+    }
+    defer tlsConn.Close()
+
+    reader := bufio.NewReader(tlsConn)
+    clientReq, err := http.ReadRequest(reader)
+    if err != nil {
+        return err
+    }
+
+    color.White("[%s] [*] Received HTTPS request: %s", time.Now().Format("01-02 15:04:05"), "https://" + clientReq.Host + clientReq.URL.Path)
+    err = ForwardHTTPRequest(tlsConn, clientReq, true)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func getTLSConfigForHost(host string) (*tls.Config, error) {
+    tlsConfig, ok := certCache.Load(host)
+    if ok {
+        return tlsConfig.(*tls.Config), nil
+    }
+
+    // Generate new server cert
+    serverCert, err := utils.GenerateServerCert(host)
+    if err != nil {
+        return nil, err
+    }
+
+    tlsConfig = &tls.Config{
+        Certificates: []tls.Certificate{*serverCert},
+    }
+    
+    certCache.Store(host, tlsConfig)
+
+    return tlsConfig.(*tls.Config), nil
+}
+
+func ForwardHTTPRequest(conn net.Conn, req *http.Request, ishttps bool) error {
+	url := req.URL.String()
+    if ishttps {
+        url = "https://" + req.Host + req.URL.Path
+    }
+	headers := headersToMap(req.Header)
     httpMethod := req.Method
-    headers := headersToMap(req.Header)
-    var err error
-    color.White("[%s] [*] Received HTTP request: %s", time.Now().Format("01-02 15:04:05"), url)
-    switch httpMethod {
+    var resp *http.Response
+	var err error
+	switch httpMethod {
     case "GET":
         resp, err = utils.Get(url,headers)
-        if err != nil {
-            return err
-        }
     case "HEAD":
         resp, err = utils.Head(url,headers)
-        if err != nil {
-            return err
-        }
     case "OPTIONS":
         resp, err = utils.Options(url,headers)
-        if err != nil {
-            return err
-        }
     case "TRACE":
         resp, err = utils.Trace(url,headers)
-        if err != nil {
-            return err
-        }
     case "POST":
         body, err := io.ReadAll(req.Body)
         if err != nil {
             return err
         }
         req.Body.Close()
-        resp, err = utils.Post(url,body,headers)
-        if err != nil {
-            return err
-        }
+        resp, err = utils.Post(url, body, headers)
     case "PUT":
         body, err := io.ReadAll(req.Body)
         if err != nil {
             return err
         }
         req.Body.Close()
-        resp, err = utils.Put(url,body,headers)
-        if err != nil {
-            return err
-        }
+        resp, err = utils.Put(url, body, headers)
     case "DELETE":
         body, err := io.ReadAll(req.Body)
         if err != nil {
             return err
         }
         req.Body.Close()
-        resp, err = utils.Delete(url,body,headers)
-        if err != nil {
-            return err
-        }
+        resp, err = utils.Delete(url, body, headers)
     default:
-        color.Red("[%s] [!] Error: Not supported %s Method!", time.Now().Format("01-02 15:04:05"), httpMethod)
-        return nil
+        return fmt.Errorf("Not supported %s Method!", httpMethod)
     }
-    defer resp.Body.Close()
+    if err != nil {
+        return err
+    }
+	defer resp.Body.Close()
     body, err := io.ReadAll(resp.Body)
     if err != nil {
         return err
@@ -171,187 +228,50 @@ func handleHTTP(conn net.Conn, req *http.Request) error {
     return nil
 }
 
-// handleHTTPS
-func handleHTTPS(conn net.Conn, req *http.Request) error {
-    defer conn.Close()
-    host := req.URL.Host
-    hostParts := strings.Split(host, ":")
-    if len(hostParts) > 0 {
-        host = hostParts[0]
-    }
-    // Generate server cert
-    serverCert, err := utils.GenerateServerCert(host)
-    if err != nil {
-        return err
-    }
-
-    tlsConfig := &tls.Config{
-        Certificates: []tls.Certificate{*serverCert},
-    }
-    
-    var resp *http.Response
-    baseurl := "https://" + req.Host
-    _, err = conn.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
-    if err != nil {
-        return err
-    }
-    
-    tlsConn := tls.Server(conn, tlsConfig)
-    err = tlsConn.Handshake()
-    if err != nil {
-        return err
-    }
-    defer tlsConn.Close()
-    
-    // Read client http request
-    reader := bufio.NewReader(tlsConn)
-    clientReq, err := http.ReadRequest(reader)
-    if err != nil {
-        return err
-    }
-
-    httpsMethod := clientReq.Method
-    httpsUrl := clientReq.URL.String()
-    headers := headersToMap(clientReq.Header)
-    url := baseurl + httpsUrl
-    color.White("[%s] [*] Received HTTPS request: %s", time.Now().Format("01-02 15:04:05"), url)
-    
-    switch httpsMethod {
-    case "GET":
-        resp, err = utils.Get(url,headers)
-        if err != nil {
-            return err
-        }
-    case "HEAD":
-        resp, err = utils.Head(url,headers)
-        if err != nil {
-            return err
-        }
-    case "OPTIONS":
-        resp, err = utils.Options(url,headers)
-        if err != nil {
-            return err
-        }
-    case "TRACE":
-        resp, err = utils.Trace(url,headers)
-        if err != nil {
-            return err
-        }
-    case "POST":
-        body, err := io.ReadAll(clientReq.Body)
-        if err != nil {
-            return err
-        }
-        clientReq.Body.Close()
-        resp, err = utils.Post(url,body,headers)
-        if err != nil {
-            return err
-        }
-    case "PUT":
-        body, err := io.ReadAll(clientReq.Body)
-        if err != nil {
-            return err
-        }
-        clientReq.Body.Close()
-        resp, err = utils.Put(url,body,headers)
-        if err != nil {
-            return err
-        }
-    case "DELETE":
-        body, err := io.ReadAll(clientReq.Body)
-        if err != nil {
-            return err
-        }
-        clientReq.Body.Close()
-        resp, err = utils.Delete(url,body,headers)
-        if err != nil {
-            return err
-        }
-    default:
-        color.Red("[%s] [!] Error: Not supported %s Method!", time.Now().Format("01-02 15:04:05"), httpsMethod)
-        return nil
-    }
-    defer resp.Body.Close()
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return err
-    }
-    resp.Body = io.NopCloser(bytes.NewReader(body))
-    responseDump, err := httputil.DumpResponse(resp, true)
-    if err != nil {
-        return err
-    }
-    _, err = tlsConn.Write(responseDump)
-    if err != nil {
-        return err
-    }
-    MitmMatchFingerprint(url, resp.StatusCode,resp.Header, body)
-    return nil
-}
-
 func MitmMatchFingerprint(url string, statuscode int, header http.Header, body []byte) {
-    var matched bool
-    cfg := config.GetConfig()
-    server := header.Get("Server")
-    if server == "" {
-        server = "None"
-    }
-    cms := "None"
-    title := utils.FetchTitle(body)
-    if title == "" {
-        title = "None"
-    }
     debody, err := DecodeBody(header.Get("Content-Encoding"), body)
     if err != nil {
         color.Red("[%s] [!] Error: %s", time.Now().Format("01-02 15:04:05"), err)
     }
     faviconurl := utils.FetchFavicon(debody)
     if strings.Contains(url, faviconurl) && statuscode == http.StatusOK {
-        for _, fingerprint := range cfg.Finger {
-            matched = matchKeywords(nil, header, title, debody, fingerprint)
-            if matched {
-                cms = fingerprint.CMS
-                if !matchedCMS[cms] {
-                    matchedCMS[cms] = true
-                    color.Green("[%s] [+] [%s] [%s] [%d] [%s] [%s]", time.Now().Format("01-02 15:04:05"), url, cms, statuscode, server, title)
-                    result := config.Result{
-                        URL:        url,
-                        CMS:        cms,
-                        Server:     server,
-                        StatusCode: statuscode,
-                        Title:      title,
-                    }
-                    output.AddResults(result)
-                }
-            }
-        }
-        err := output.WriteOutputs()
-        if err != nil {
-            color.Red("[%s] [!] Error: %s", time.Now().Format("01-02 15:04:05"), err)
-        }
+        matchfingerprint(url, statuscode, nil, header, debody)
     } else {
-        for _, fingerprint := range cfg.Finger {
-            matched = matchKeywords(debody, header, title, nil, fingerprint)
-            if matched {
-                cms = fingerprint.CMS
-                if !matchedCMS[cms] {
-                    matchedCMS[cms] = true
-                    color.Green("[%s] [+] [%s] [%s] [%d] [%s] [%s]", time.Now().Format("01-02 15:04:05"), url, cms, statuscode, server, title)
-                    result := config.Result{
-                        URL:        url,
-                        CMS:        cms,
-                        Server:     server,
-                        StatusCode: statuscode,
-                        Title:      title,
-                    }
-                    output.AddResults(result)
+        matchfingerprint(url, statuscode, debody, header, nil)
+    }
+}
+
+func matchfingerprint(url string, statuscode int, body []byte, header http.Header, favicon []byte) {
+    var matched bool
+    cms := "None"
+    server := header.Get("Server")
+    if server == "" {
+        server = "None"
+    }
+    title := utils.FetchTitle(body)
+    if title == "" {
+        title = "None"
+    }
+    for _, fingerprint := range config.Config.Finger {
+        matched = matchKeywords(body, header, title, favicon, fingerprint)
+        if matched {
+            cms = fingerprint.CMS
+            if _, loaded := matchedCMS.LoadOrStore(cms, true); !loaded {
+                color.Green("[%s] [+] [%s] [%s] [%d] [%s] [%s]", time.Now().Format("01-02 15:04:05"), url, cms, statuscode, server, title)
+                result := config.Result{
+                    URL:        url,
+                    CMS:        cms,
+                    Server:     server,
+                    StatusCode: statuscode,
+                    Title:      title,
                 }
+                output.AddResults(result)
             }
         }
-        err := output.WriteOutputs()
-        if err != nil {
-            color.Red("[%s] [!] Error: %s", time.Now().Format("01-02 15:04:05"), err)
-        }
+    }
+    err := output.WriteOutputs()
+    if err != nil {
+        color.Red("[%s] [!] Error: %s", time.Now().Format("01-02 15:04:05"), err)
     }
 }
 
@@ -359,7 +279,6 @@ func DecodeBody(contentEncoding string, body []byte) ([]byte, error) {
     var reader io.ReadCloser
     var err error
 
-    // 根据 content-encoding 创建相应的解码器
     switch strings.ToLower(contentEncoding) {
     case "gzip":
         reader, err = gzip.NewReader(bytes.NewReader(body))
