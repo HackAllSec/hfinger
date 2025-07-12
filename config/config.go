73e902bfdaf5ce8e040ee0cfe1e76a098f64f563
NHPT <42366434+NHPT@users.noreply.github.com>
2025-07-13 02:01:40 +0800

    v1.0.8

--- config/config.go
+++ config/config.go
@@ -38,7 +38,7 @@ type LastResponse struct {
 var (
     Config *FingerprintConfig
     once   sync.Once
-    Version = "v1.0.7"
+    Version = "v1.0.8"
     CertsDir   = "certs"
     CaCertFile = "ca.crt"
     CaKeyFile  = "ca.key"
